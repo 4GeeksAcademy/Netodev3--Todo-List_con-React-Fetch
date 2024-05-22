cad153a44d762f519220e2979e260ab322975952
Net Stark <105382120+Netodev3@users.noreply.github.com>
2024-05-22 16:23:15 +0000

    Fin con fallo de API

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,139 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleKey = (event) => {
+    const updateValue = {
+      done: true,
+      label: inputValue,
+    };
+    if (event.key === "Enter") {
+      setTodos([...todos, updateValue]);
+      updateTodo();
+      setInputValue("");
+    }
+  };
+
+  const deleteEvent = (index) => {
+    const listaActualizada = todos.filter(
+      (t, currentIndex) => index !== currentIndex
+    );
+    setTodos(listaActualizada);
+  };
+
+  const deleteEventAll = () => {
+    setTodos([]);
+  };
+
+  const urlTodos =
+    "https://playground.4geeks.com/apis/fake/todos/user/Netodev3";
+
+  useEffect(() => {
+    getTask();
+  }, []);
+
+  const getTask = () => {
+    fetch(urlTodos)
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((err) => err);
+  };
+
+  useEffect(() => {
+    newTask();
+  }, [todos]);
+
+  const newTask = () => {
+    fetch(urlTodos, {
+      method: "POST",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+
+  const updateTodo = () => {
+    const newTodo = {
+      done: false,
+      label: inputValue,
+    };
+    fetch(urlTodos, {
+      method: "PUT",
+      body: JSON.stringify([...todos, newTodo]),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+
+  return (
+    <div className="container">
+      <h1>ToDo's</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            placeholder="What do you need to do?"
+            value={inputValue}
+            onChange={handleChange}
+            onKeyDown={handleKey}
+          />
+        </li>
+        {todos.map((value, index) => (
+          <li
+            key={index}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {value.label}
+            <button
+              className="btn btn-light"
+              onClick={() => deleteEvent(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="divCenter d-flex justify-content-center m-3">
+        <button className="btn btn-success" onClick={deleteEventAll}>
+          Delete All
+        </button>
+      </div>
+
+      <div className="etiqueta form-control col-2" style={{ width: "100px" }}>
+        {todos.length} item left
+      </div>
+    </div>
+  );
 };
 
 export default Home;
